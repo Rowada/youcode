3bd6d6328250f4784975061e3e9ad28279177f8f
Josue <96113041+Rowada@users.noreply.github.com>
2024-08-20 10:25:36 +0200

    T5 : add course users view

---
+++ app/admin/courses/[courseId]/page.tsx
@@ -0,0 +1,125 @@
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/components/layout/Layout";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getRequiredAuthSession } from "@/lib/auth";
+import { gettCourse } from "./course.query";
+import { Typography } from "@/components/ui/Typography";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export default async function CoursePage({
+  params,
+  searchParams,
+}: {
+  params: {
+    courseId: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const page = Number(searchParams.page) || 1;
+
+  const session = await getRequiredAuthSession();
+
+  const course = await gettCourse({
+    courseId: params.courseId,
+    userId: session.user.id,
+    userPage: page,
+  });
+
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Courses</LayoutTitle>
+        <LayoutContent className="flex flex-col gap-4 p-4 lg:flex-row">
+          <Card className="flex-[2]">
+            <CardHeader>
+              <CardTitle>Users</CardTitle>
+            </CardHeader>
+
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableHead>Image</TableHead>
+                  <TableHead>Name</TableHead>
+                </TableHeader>
+                <TableBody>
+                  {course.users?.map((user) => (
+                    <TableRow>
+                      <TableCell>
+                        <Avatar className="rounded">
+                          <AvatarFallback>{user.email?.[0]}</AvatarFallback>
+                          {user.image && (
+                            <AvatarImage
+                              src={user.image}
+                              alt={user.email ?? ""}
+                            />
+                          )}
+                        </Avatar>
+                      </TableCell>
+                      <TableCell>
+                        <Typography
+                          as={Link}
+                          variant="large"
+                          href={`/admin/users/${user.id}`}
+                        >
+                          {user.email}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+
+              {/* pagination */}
+            </CardContent>
+          </Card>
+
+          <Card className="flex-1">
+            <CardHeader className="flex-row items-center gap-4 space-y-0">
+              <Avatar className="rounded">
+                <AvatarFallback>{course.name?.[0]}</AvatarFallback>
+                {course.image && (
+                  <AvatarImage src={course.image} alt={course.name ?? ""} />
+                )}
+              </Avatar>
+              <CardTitle>{course.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-1">
+              <Typography>{course._count?.users} users</Typography>
+              <Typography>{course._count?.lessons} lessons</Typography>
+              <Link
+                href={`/admin/courses/${course.id}/edit`}
+                className={buttonVariants({
+                  variant: "outline",
+                })}
+              >
+                Edit
+              </Link>{" "}
+              <Link
+                href={`/admin/courses/${course.id}/lessons`}
+                className={buttonVariants({
+                  variant: "outline",
+                })}
+              >
+                Edit lessons
+              </Link>
+            </CardContent>
+          </Card>
+        </LayoutContent>
+      </LayoutHeader>
+    </Layout>
+  );
+}
