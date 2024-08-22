2bacc79d224dc9bee50ad6d4caed85e4c340cc99
Josue <96113041+Rowada@users.noreply.github.com>
2024-08-22 11:45:32 +0200

    T6 : add lessons request and lessons view

---
+++ app/admin/courses/[courseId]/lessons/lessons.query.ts
@@ -0,0 +1,25 @@
+// crée une requête prisma pour récupérer les lessons d'un cours
+
+import { prisma } from "@/lib/prisma";
+
+export const getLessons = async ({
+  courseId,
+  userId,
+}: {
+  courseId: string;
+  userId: string;
+}) => {
+  const lessons = await prisma.course.findFirst({
+    where: {
+      id: courseId,
+      creatorId: userId,
+    },
+    select: {
+      id: true,
+      name: true,
+      lessons: true,
+    },
+  });
+
+  return lessons;
+};
---
+++ app/admin/courses/[courseId]/lessons/page.tsx
@@ -0,0 +1,49 @@
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/components/layout/Layout";
+import { getRequiredAuthSession } from "@/lib/auth";
+import { getLessons } from "./lessons.query";
+import { notFound } from "next/navigation";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Typography } from "@/components/ui/Typography";
+
+export default async function CourseLessonsPage({
+  params,
+}: {
+  params: { courseId: string };
+}) {
+  const session = await getRequiredAuthSession();
+
+  const lessons = await getLessons({
+    courseId: params.courseId,
+    userId: session.user.id,
+  });
+
+  if (!lessons) {
+    notFound();
+  }
+
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Lessons - {lessons.name}</LayoutTitle>
+        <LayoutContent className="flex flex-col gap-4 p-4 lg:flex-row">
+          <Card className="flex-[2]">
+            <CardHeader>
+              <CardTitle>Lessons</CardTitle>
+            </CardHeader>
+
+            <CardContent className="flex flex-col gap-2">
+              {lessons.lessons.map((lesson) => (
+                <span>{lesson.name}</span>
+              ))}
+            </CardContent>
+          </Card>
+        </LayoutContent>
+      </LayoutHeader>
+    </Layout>
+  );
+}
