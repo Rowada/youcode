be738df25292624bbaeeb94683e5edb6dedfc7d8
Josue <96113041+Rowada@users.noreply.github.com>
2024-09-11 11:02:06 +0200

    T11 : add courses update

---
+++ app/admin/courses/[courseId]/edit/CourseForm.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useZodForm,
+} from "@/components/ui/form";
+import { CourseFormSchema } from "./course.schema";
+import { useRouter } from "next/navigation";
+import { courseActionEdit } from "./course.action";
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+export type CourseFormProps = {
+  defaultValue?: CourseFormSchema & {
+    id: string;
+  };
+};
+
+export const CourseForm = ({ defaultValue }: CourseFormProps) => {
+  const form = useZodForm({
+    schema: CourseFormSchema,
+    defaultValues: defaultValue,
+  });
+
+  const router = useRouter();
+
+  return (
+    <Form
+      form={form}
+      onSubmit={async (values) => {
+        // console.log(values);
+
+        if (defaultValue?.id) {
+          // console.log("update");
+          const result = await courseActionEdit({
+            courseId: defaultValue.id,
+            data: values,
+          });
+
+          if (result?.data) {
+            toast.success(result.data);
+            router.push(`/admin/courses/${defaultValue.id}`);
+            router.refresh();
+            return;
+          }
+
+          toast.error("Some error occured", {
+            description: result?.serverError,
+          });
+          return;
+        } else {
+          // create course
+        }
+      }}
+    >
+      <FormField
+        control={form.control}
+        name="image"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Image</FormLabel>
+            <FormControl>
+              <Input placeholder="https://googleimage.com" {...field} />
+            </FormControl>
+            <FormDescription>
+              Host and use an image. You can use {""}
+              <Link href="https://imgur.com/" target="_blank">
+                imgur
+              </Link>{" "}
+              to host your image.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Course name" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Description</FormLabel>
+            <FormControl>
+              <Textarea placeholder="## Some title" {...field} />
+            </FormControl>
+            <FormDescription>Markdown is supported.</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
+};
--- app/admin/courses/[courseId]/edit/page.tsx
+++ app/admin/courses/[courseId]/edit/page.tsx
@@ -1,3 +1,51 @@
-export default async function CourseEditPage() {
-  return <div>Course Edit Page</div>;
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/components/layout/Layout";
+import { Card, CardContent } from "@/components/ui/card";
+import { getRequiredAuthSession } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import { CourseForm } from "./CourseForm";
+
+export default async function CourseEditPage({
+  params,
+}: {
+  params: { courseId: string };
+}) {
+  const session = await getRequiredAuthSession();
+
+  const course = await prisma.course.findUnique({
+    where: {
+      id: params.courseId,
+      creatorId: session.user.id,
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      image: true,
+    },
+  });
+
+  if (!course) {
+    notFound();
+  }
+
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Courses</LayoutTitle>
+        <LayoutContent>
+          <Card className="flex-[2]">
+            <CardContent className="mt-6">
+              <CourseForm defaultValue={course} />
+            </CardContent>
+          </Card>
+        </LayoutContent>
+      </LayoutHeader>
+    </Layout>
+  );
 }
