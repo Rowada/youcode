bde5e75e0bdc657b1ec8972d3c1ca6351b377d12
Josue <96113041+Rowada@users.noreply.github.com>
2024-09-12 13:57:26 +0200

    T13 : update CourseLessonsPage to enable lesson creation with server action and integrate SubmitButton for better user experience

--- app/admin/courses/[courseId]/lessons/page.tsx
+++ app/admin/courses/[courseId]/lessons/page.tsx
@@ -1,21 +1,16 @@
+import { SubmitButton } from "@/components/form/SubmitButton";
 import {
   Layout,
   LayoutContent,
   LayoutHeader,
   LayoutTitle,
 } from "@/components/layout/Layout";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getRequiredAuthSession } from "@/lib/auth";
-import { getLessons } from "./lessons.query";
-import { notFound } from "next/navigation";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { prisma } from "@/lib/prisma";
+import { notFound, redirect } from "next/navigation";
 import { AdminLessonList } from "./AdminLessonList";
-import { Button } from "@/components/ui/button";
+import { getLessons } from "./lessons.query";
 
 export default async function CourseLessonsPage({
   params,
@@ -36,27 +31,57 @@ export default async function CourseLessonsPage({
   return (
     <Layout>
       <LayoutHeader>
-        <LayoutTitle>Lessons - {lessons.name}</LayoutTitle>
-        <LayoutContent className="flex flex-col gap-4 p-4 lg:flex-row">
-          <Card className="flex-[2]">
-            <CardHeader>
-              <CardTitle>Lessons</CardTitle>
-            </CardHeader>
-
-            <CardContent className="flex flex-col gap-2">
-              {lessons.lessons.map((lesson) => (
-                <AdminLessonList key={lesson.id} lesson={lesson} />
-              ))}
-            </CardContent>
-
-            <CardFooter>
-              <Button variant={"secondary"} className="w-full">
-                Create lesson
-              </Button>
-            </CardFooter>
-          </Card>
-        </LayoutContent>
+        <LayoutTitle>Lessons - {lessons.name}</LayoutTitle>{" "}
       </LayoutHeader>
+      <LayoutContent className="flex flex-col gap-4 p-4 lg:flex-row">
+        <Card className="flex-[2]">
+          <CardHeader>
+            <CardTitle>Lessons</CardTitle>
+          </CardHeader>
+
+          <CardContent className="flex flex-col gap-2">
+            {lessons.lessons.map((lesson) => (
+              <AdminLessonList key={lesson.id} lesson={lesson} />
+            ))}
+
+            <form>
+              <SubmitButton
+                size="sm"
+                variant="secondary"
+                className="w-full"
+                formAction={async () => {
+                  "use server";
+
+                  const session = await getRequiredAuthSession();
+
+                  const courseId = params.courseId;
+
+                  await prisma.course.findFirstOrThrow({
+                    where: {
+                      creatorId: session.user.id,
+                      id: courseId,
+                    },
+                  });
+
+                  const lesson = await prisma.lesson.create({
+                    data: {
+                      name: "New lesson",
+                      rank: "aaaa",
+                      state: "HIDDEN",
+                      content: "## New lesson content",
+                      courseId: courseId,
+                    },
+                  });
+
+                  redirect(`/admin/courses/${courseId}/lessons/${lesson.id}`);
+                }}
+              >
+                Create lesson
+              </SubmitButton>
+            </form>
+          </CardContent>
+        </Card>
+      </LayoutContent>
     </Layout>
   );
 }
